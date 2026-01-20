05937ade55d51aa957d7c36d1c45f8b2eb9c942b
coder-naveen18 <codersahu18@gmail.com>
2026-01-20 15:09:32 +0530

    Refactor patient detail update logic to raise exceptions for invalid serializer

--- Hospital/views.py
+++ Hospital/views.py
@@ -47,10 +47,10 @@ def patient_detail(request, pk):
 
     if request.method == 'PUT': 
         serializer = PatientSerializer(patient, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
 
     if request.method == 'DELETE': 
         patient.delete()
